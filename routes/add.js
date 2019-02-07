f606227a01ca7ab394689c80d69fe21cd76206ca
choiwhmarco <whchoi@ucsd.edu>
2019-02-07 11:00:18 -0800

    update

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,12 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	var addFriend = {
+		'name':request.query.name,
+		'description': request.query.description,
+		"imageURL": 'http://lorempixel.com/500/500/people'
+
+	};
+	data['friends'].push(newFriend);
+	res.render('add', data);
  }
